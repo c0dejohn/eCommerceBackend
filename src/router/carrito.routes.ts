71399edcc3e7d5bd7ef34e263746af4b68bb279a
c0dejohn <jemanyoma07@gmail.com>
2022-02-05 23:29:19 -0500

    refactor on routes & controller

--- src/router/carrito.routes.ts
+++ src/router/carrito.routes.ts
@@ -2,49 +2,11 @@ import { Router } from "express";
 const cartRouter = Router();
 import Carrito from "../controller/carrito";
 
-const producto = new Carrito("carrito.txt");
+const carrito = new Carrito();
 
-cartRouter.get("/:id/productos", async (req, res) => {
-	try {
-		const id = req.params.id ?? 0;
-		const result = await producto.mostrarProducto(parseInt(id, 10));
+cartRouter.get("/:id/productos", carrito.getById);
+cartRouter.post("/", carrito.addProduct);
+cartRouter.put(`/:id`, carrito.update);
+cartRouter.delete(`/:id`, carrito.destroy);
 
-		result !== undefined
-			? res.send(result)
-			: res.send({ error: "producto no encontrado" });
-	} catch (error) {
-		res.send(error);
-	}
-});
-
-cartRouter.post("/", async (req: any, res: any) => {
-	try {
-		const { id, title, price } = await req.body;
-		const result = await producto.agregarProducto(id, title, price);
-		result !== undefined ? res.status(201).send(result) : res.send(result);
-	} catch (error) {
-		res.send(error);
-	}
-});
-
-cartRouter.put(`/:id`, async (req: any, res: any) => {
-	try {
-		const { title, price, thumbnail } = await req.body;
-		const id = await req.params.id;
-		const payload = await producto.actualizarProducto(
-			title,
-			price,
-			thumbnail,
-			id
-		);
-		res.send(payload);
-	} catch (error) {
-		res.send(error);
-	}
-});
-
-cartRouter.delete(`/:id`, (req: any, res: any) => {
-	const id = req.params.id;
-	res.send(producto.eliminarProducto(parseInt(id, 10)));
-});
 export default cartRouter;
